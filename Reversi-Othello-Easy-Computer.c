e4c58964c4d86f23cf4d165ef9876fcfc2ba9c1f
Mike Shi <mikehxshi@gmail.com>
2017-11-17 14:34:05 -0500

    Update Reversi-Othello-Easy-Computer.c

--- Reversi-Othello-Easy-Computer.c
+++ Reversi-Othello-Easy-Computer.c
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   Lab7.c
- * Author: shimike
- *
- * Created on March 20, 2017, 12:34 PM
- */
 
 #include <stdio.h>
 #include <stdlib.h>
